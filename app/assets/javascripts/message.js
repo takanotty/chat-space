f260de09eda28a9d901d3f74ba5720265eb7619a
takanotty <i.kunot.shukatsu@gmail.com>
2019-12-08 11:48:14 +0900

    delete-comment

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,7 +10,6 @@ $(function(){
       `</div>` +
     `</div>` 
     if (message.content && message.image) {
-      //data-idが反映されるようにしている
       var html = format +
         `<div class="lower-message">` +
           `<p class="lower-message__content">` +
@@ -20,7 +19,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
       var html = format +
         `<div class="lower-message">` +
           `<p class="lower-message__content">` +
@@ -29,7 +27,6 @@ $(function(){
         `</div>` +
       `</div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
       var html = format +
         `<div class="lower-message">` +
           `<img src="` + message.image + `" class="lower-message__image" >` +
@@ -65,21 +62,16 @@ $(function(){
   var reloadMessages = function() {
     last_message_id = $('.message').last().data('message_id');
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "./api/messages",
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
